0dfa56b7d8545b4e295a07a52a7ffbe750e92d46
Tomislav Markovski <tomislav@trinsic.id>
2021-07-08 09:14:39 -0400

    Update p256 key with correct fingerprint encoding based on test vectors

--- src/lib.rs
+++ src/lib.rs
@@ -181,7 +181,7 @@ impl TryFrom<&str> for KeyPair {
             [0xed, 0x1] => KeyPair::Ed25519(Ed25519KeyPair::from_public_key(&pub_key[2..])),
             [0xec, 0x1] => KeyPair::X25519(X25519KeyPair::from_public_key(&pub_key[2..])),
             [0xee, 0x1] => KeyPair::Bls12381G1G2(Bls12381KeyPair::from_public_key(&pub_key[2..])),
-            [0x12, 0x0] => KeyPair::P256(P256KeyPair::from_public_key(&pub_key[3..])),
+            [0x80, 0x24] => KeyPair::P256(P256KeyPair::from_public_key(&pub_key[2..])),
             [0xe7, 0x0] => KeyPair::Secp256k1(Secp256k1KeyPair::from_public_key(&pub_key[2..])),
             _ => unimplemented!("unsupported key type"),
         });
